374e20421358108608a774f6df3f21aab467e56c
Nilsen89 <nilsendesign@live.no>
2017-03-31 01:23:54 +0200

    updated front picture

--- rorsvis/src/Components/FrontText.js
+++ rorsvis/src/Components/FrontText.js
@@ -14,18 +14,22 @@ class FrontText extends Component {
 	
 		return(
 			<div style={boxStyle}>
-				<div style={{fontSize:"24", fontWeight:"bold"}}>Our Servies:</div>
+				<div style={{fontSize:"24", fontWeight:"bold", marginBottom:"15px"}}>Our Servies:</div>
 				<Row>
 					<Col md={4} xs={6} mdOffset={2}>
-						<span style={{fontSize:"18", fontWeight:"bold"}}>Welding</span>
-						<p>We offer our experienced services in welding to you and your firm.
-						Rør & Sveis have many years experience within welding and take pride in
-						doing good work.</p>
+						<div style={{border:"1px dashed black"}}>
+							<span style={{fontSize:"18", fontWeight:"bold"}}>Welding</span>
+							<p>We offer our experienced services in welding to you and your firm.
+							Rør & Sveis have many years experience within welding and take pride in
+							doing good work.</p>
+						</div>
 					</Col>
 					<Col md={4} xs={6}>
-						<span style={{fontSize:"18", fontWeight:"bold"}}>Piping</span>
-						<p>Rør & Sveis have personell with decade of experience with piping, and other
-						services related pipes, we can do the work needed to be done.</p>
+						<div style={{border:"1px dashed black"}}>
+							<span style={{fontSize:"18", fontWeight:"bold"}}>Piping</span>
+							<p>Rør & Sveis have personell with decade of experience with piping, and other
+							services related pipes, we can do the work needed to be done.</p>
+						</div>
 					</Col>
 				</Row>
 			</div>
