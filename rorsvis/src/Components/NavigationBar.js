9952e33f69c86251d693f9a17a470d08b0fe6620
Rasmus Volvesen <Rasmus Volvesen>
2017-04-03 03:44:35 +0200

    Fiksa språk

--- rorsvis/src/Components/NavigationBar.js
+++ rorsvis/src/Components/NavigationBar.js
@@ -2,7 +2,7 @@
  * Created by Rasmus on 30.03.2017.
  */
 import React, {Component} from 'react'
-import {Navbar, Nav} from 'react-bootstrap'
+import {Navbar, Nav, NavItem} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
 import './NavigationBar.css'
 import ListItemLink from './ListItemLink'
@@ -10,24 +10,29 @@ import {Image} from 'react-bootstrap'
 import {NavLink} from 'react-router-dom'
 
 class NavigationBar extends Component {
+
+
     render() {
 
-		let flagger = "eng"
+        function handleClick(e) {
+            if(localStorage.getItem("lang") === "eng") {
+                localStorage.setItem("lang", "no")
+            } else {
+                localStorage.setItem("lang", "eng")
+            }
+        }
+
+
+		let flagger = "eng";
 		if(localStorage.getItem("lang") === "eng") {
 			flagger = "no"
 		}
 
         let file = require("../Data/"+localStorage.getItem("lang")+"/NavigationBar.json");
         let imgsrc = require("../images/"+flagger+".png");
-		let flagImg = <Image className="english" src={imgsrc} alt="flag" onClick={handleClick}/>
+		let flagImg = <Image className="english" src={imgsrc} alt="flag"/>;
 
-		function handleClick(e) {
-			if(localStorage.getItem("lang") === "eng") {
-				localStorage.setItem("lang", "no")
-			} else {
-				localStorage.setItem("lang", "eng")
-			}
-		}
+
         return(
             <Navbar className="navbar" fixedTop>
                 <Navbar.Header>
@@ -41,11 +46,8 @@ class NavigationBar extends Component {
                         <ListItemLink className="link" to="/About">{file[0].about}</ListItemLink>
                         <ListItemLink className="link" to="/Contact">{file[0].contact}</ListItemLink>
                         <li>
-                            <NavLink to="/">
-                                {flagImg}
-                            </NavLink>
+                            <Link to="/" onClick={() => handleClick()}>{flagImg}</Link>
                         </li>
-
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
